799eff951fcda84f0495c532af778086e7dc5801
Will <vjblan@gmail.com>
2024-11-27 19:01:09 -0300

    Cron tasks and API rate limit

--- app/Console/Commands/SendEventReminders.php
+++ app/Console/Commands/SendEventReminders.php
@@ -3,6 +3,7 @@
 namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
+use Illuminate\Support\Str;
 
 class SendEventReminders extends Command
 {
@@ -18,13 +19,28 @@ class SendEventReminders extends Command
      *
      * @var string
      */
-    protected $description = 'Command description';
+    protected $description = 'Send notifications to all events attendees that event starts soon';
 
     /**
      * Execute the console command.
      */
     public function handle()
     {
-        //
+        $events = \App\Models\Event::with('attendees.user')
+        ->whereBetween('start_time', [now(), now()->addDay()])
+        ->get();
+
+        $eventsCount = $events->count();
+        $eventLabel = Str::plural('event', $eventsCount);
+
+        $this->info("Found {$eventsCount} {$eventLabel} events.");
+
+        $events->each(
+            fn($event) => $event->attendees->each(
+                fn($attendee) => $this->info("Notifying the user {$attendee->user->id}")
+            )
+        );
+
+        $this->info('Reminders sent.');
     }
 }
